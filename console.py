7e6816a59ce0990415c7d3297db3db76cbb885fd
timidgeek <5568@holbertonstudents.com>
2023-02-05 17:39:25 -0800

    authors page, command interpreter for task 7

---
+++ console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""console.py program, contains the entry point
+    of the command interpreter"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    while input == "":
+        pass
+
+    def do_EOF(self, line):
+        "Exit"
+        return True
+
+    def do_quit(self, line):
+        "Quit command to exit the program\n"
+        raise SystemExit
+
+    """help action is provided by default,
+        but should be kept updated and documented
+        as we work through tasks"""
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
