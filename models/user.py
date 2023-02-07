4f2d5fcb012d78f7d7ad54f30c697b8c18247d95
teylorchapman <teylor.chapman2@gmail.com>
2023-02-07 10:22:19 -0800

    user class :)

---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A class called user that inherits from BaseModel"""
+from models.base_model import BaseModel
+
+
+class User:
+    """the user class"""
+    first_name = ""
+    last_name = ""
+    email = ""
+    password = ""
