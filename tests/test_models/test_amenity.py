16d08f544c6510fd0c3e70484ce1b198519da628
teylorchapman <5535@holbertonstudent.com>
2023-02-08 09:29:02 -0800

    anotha one

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,35 +1,49 @@
 #!/usr/bin/python3
 """AMENITY MODULE TESTS"""
-from models.amenity import Amenity
-import pep8
 import unittest
+from datetime import datetime
+import time
+from models.amenity import Amenity
+import re
+import json
+from models.engine.file_storage import FileStorage
+import os
+from models import storage
+from models.base_model import BaseModel
+
 
 class TestAmenity(unittest.TestCase):
-    """Test cases for Amenity"""
+
+    """Test Cases for the Amenity class."""
 
     def setUp(self):
-        """Sets up objects for testing"""
-        self.amenityOne = Amenity()
-        self.amenityTwo = Amenity()
+        """Sets up test methods."""
+        pass
 
-    def test_pep8(self):
-        """Tests for pep8 style"""
-        pep8style = pep8.StyleGuide(quite=True)
-        result = pep8style.check_files(["./models/amenity.py"])
-        self.assertEqual(result.total_errors, 0)
+    def tearDown(self):
+        """Tears down test methods."""
+        self.resetStorage()
+        pass
 
-    def test_type(self):
-        """Tests the type"""
-        self.assertEqual(type(self.amenityOne.name), str)
-        self.assertEqual(type(self.amenityTwo.name), str)
-        self.assertIsInstance(self.amenityOne, Amenity)
-        self.assertIsInstance(self.amenityTwo, Amenity)
+    @classmethod
+    def resetStorage(self):
+        """Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
 
-    def test_attributes(self):
-        """Tests the attributes"""
-        self.assertTrue(hasattr(self.amenityOne, "name"))
-        self.assertFalse(hasattr(self.amenityOne, "user_id"))
-        self.assertTrue(self.amenityOne.id != self.amenityTwo.id)
+    def test_8_instantiation(self):
+        """Tests instantiation of Amenity class."""
+
+        b = Amenity()
+        self.assertEqual(str(type(b)), "<class 'models.amenity.Amenity'>")
+        self.assertIsInstance(b, Amenity)
+        self.assertTrue(issubclass(type(b), BaseModel))
+        self.assertTrue(hasattr(b, "id"))
+        self.assertTrue(hasattr(b, "created_at"))
+        self.assertTrue(hasattr(b, "updated_at"))
+        self.assertIn('name', dir(Amenity))
+        self.assertEqual(str, type(Amenity.name))
 
 if __name__ == "__main__":
     unittest.main()
